dfdfb48a8977cace765f0ee0c78ea36eab0d9a0d
Dave Roverts <me@daveroverts.nl>
2018-06-06 22:01:20 +0200

    Adds relations between Airport and Booking

--- app/Airport.php
+++ app/Airport.php
@@ -9,10 +9,19 @@ class Airport extends Model
     protected $primaryKey = 'icao';
     public $incrementing = false;
     protected $keyType = 'string';
+
     /**
      * Indicates if the model should be timestamped.
      *
      * @var bool
      */
     public $timestamps = false;
+
+    public function bookingsDep() {
+        return $this->belongsToMany(Booking::class, 'events', 'dep');
+    }
+
+    public function bookingsArr() {
+        return $this->belongsToMany(Booking::class, 'events', 'arr');
+    }
 }
--- app/Booking.php
+++ app/Booking.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    public function airportDep() {
+        return $this->hasOne(Airport::class, 'dep', 'icao');
+    }
+
+    public function airportArr() {
+        return $this->hasOne(Airport::class, 'arr', 'icao');
+    }
 }
