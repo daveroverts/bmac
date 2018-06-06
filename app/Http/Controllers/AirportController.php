ce5f13f3405e9f0685fcfdacc9d8db4bfe0b4402
Dave Roverts <me@daveroverts.nl>
2018-06-06 23:14:14 +0200

    Adds Airport, Event and Booking Controllers with routes

---
+++ app/Http/Controllers/AirportController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Airport;
+use Illuminate\Http\Request;
+
+class AirportController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Airport  $airport
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Airport $airport)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Airport  $airport
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Airport $airport)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Airport  $airport
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Airport $airport)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Airport  $airport
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Airport $airport)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::resource('admin/airport', 'AirportController');
+Route::resource('admin/event', 'EventController');
+Route::resource('booking', 'BookingController');
