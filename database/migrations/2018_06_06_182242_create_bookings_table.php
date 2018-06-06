4c420fa8b7bf02a0af80543206c2b144812d7749
Dave Roverts <me@daveroverts.nl>
2018-06-06 20:38:38 +0200

    Adds bookings migration as last migration. Adds foreign keys
    Closes #1

--- database/migrations/2018_06_06_182242_create_bookings_table.php
+++ database/migrations/2018_06_06_182242_create_bookings_table.php
@@ -18,24 +18,30 @@ class CreateBookingsTable extends Migration
             $table->unsignedInteger('event_id');
             $table->unsignedInteger('reservedBy_id')->nullable();
             $table->unsignedInteger('bookedBy_id')->nullable();
-            $table->string('callsign',7)->nullable();
-            $table->string('acType',4)->nullable();
-            $table->string('selCal',5);
+            $table->string('callsign', 7)->nullable();
+            $table->string('acType', 4)->nullable();
+            $table->string('selCal', 5);
             $table->string('dep')->nullable();
             $table->string('arr')->nullable();
             $table->time('ctot')->nullable();
             $table->string('oceanicFL')->nullable();
             $table->timestamps();
+
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('reservedBy_id')->references('id')->on('users');
+            $table->foreign('bookedBy_id')->references('id')->on('users');
+            $table->foreign('dep')->references('icao')->on('airports');
+            $table->foreign('arr')->references('icao')->on('airports');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('bookings');
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down()
+        {
+            Schema::dropIfExists('bookings');
+        }
     }
-}
