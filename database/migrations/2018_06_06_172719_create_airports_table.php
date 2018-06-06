fef0850ac084ad676831aea422d1cc6ca3162740
Dave Roverts <me@daveroverts.nl>
2018-06-06 19:30:32 +0200

    Adds airports migration

---
+++ database/migrations/2018_06_06_172719_create_airports_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAirportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('airports', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('icao');
+            $table->string('iata');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('airports');
+    }
+}
