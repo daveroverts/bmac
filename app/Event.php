08b80f02bb7cad8804946443da5b5213f73565cf
Dave Roverts <me@daveroverts.nl>
2018-06-07 22:47:47 +0200

    Adds $dates to Event model

--- app/Event.php
+++ app/Event.php
@@ -6,6 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'startEvent',
+        'endEvent',
+        'startBooking',
+        'endBooking'
+    ];
+
     public function bookings() {
         return $this->belongsToMany(Booking::class);
     }
