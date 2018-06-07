4f2077482773c9b7ceb9b6ecd26bf77cb9877d94
Dave Roverts <me@daveroverts.nl>
2018-06-07 22:03:51 +0200

    Disables edit button. Changes add button

--- resources/views/airport/overview.blade.php
+++ resources/views/airport/overview.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
     <h2>Airports Overview</h2>
-    <p><a href="{{ route('airport.create') }}" class="btn btn-primary"><i class="fa fa-user-plus"></i> Add new Airport</a></p>
+    <p><a href="{{ route('airport.create') }}" class="btn btn-primary"><i class="fa fa-plus"></i> Add new Airport</a></p>
     <table class="table table-hover">
         <thead><tr>
             <th>ICAO</th>
             <th>IATA</th>
-            <th>Edit</th>
+            <th>Edit (doesn't work at this time)</th>
         </tr></thead>
         @forelse($airports as $airport)
             <tr>
@@ -15,7 +15,7 @@
                 <td>{{ $airport->iata }}</td>
                 <td>
                     <form action="{{ route('airport.edit', $airport->icao) }}">
-                        <button class="btn btn-primary"><i class="fa fa-edit"></i> Edit Airport</button>
+                        <button class="btn btn-primary" disabled><i class="fa fa-edit"></i> Edit Airport</button>
                         @csrf
                     </form>
                 </td>
