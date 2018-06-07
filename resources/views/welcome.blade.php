2944fad10a00c6e8465a30a260352afd0e07d2a1
Dave Roverts <me@daveroverts.nl>
2018-06-07 21:02:47 +0200

    Changes title on welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Book me a Cookie
                 </div>
 
                 <div class="links">
