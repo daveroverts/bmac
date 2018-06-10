2ef77a8a1f6d8f42a4b15f03ed8a894d5a540499
Dave Roverts <me@daveroverts.nl>
2018-06-10 13:06:15 +0200

    Adds a few comments in booking.overview

--- resources/views/booking/overview.blade.php
+++ resources/views/booking/overview.blade.php
@@ -25,7 +25,9 @@
                 <td>{{ $booking->callsign ? $booking->callsign : '-' }}</td>
                 <td>{{ $booking->acType ? $booking->acType : '-' }}</td>
                 <td>
+                    {{--Check if booking has been booked--}}
                     @if(isset($booking->bookedBy_id))
+                        {{--Check if booking has been booked by current user--}}
                         @if(Auth::check() && $booking->bookedBy_id == Auth::id())
                             <a href="{{ route('booking.edit',$booking->id) }}">My booking</a>
                         @else
@@ -33,13 +35,16 @@
                         @endif
 
                     @elseif(isset($booking->reservedBy_id))
+                        {{--Check if a booking has been reserved--}}
                         @if(Auth::check() && $booking->reservedBy_id == Auth::id())
+                            {{--Check if a booking has been reserved by current user--}}
                             <a href="{{ route('booking.edit',$booking->id) }}">My Reservation</a>
                         @else
                             Reserved {{Auth::check() && Auth::user()->isAdmin() ? '['.$booking->reservedBy->vatsim_id .']' : ''}}
                         @endif
                     @else
                         @if(Auth::check())
+                            {{--Check if user is logged in to generate a clickable link--}}
                             <a href="{{ route('booking.edit',$booking->id) }}">
                         @endif
                         AVAIL
