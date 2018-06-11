b146c20d7a025c8bdc50f2b3ef904ea42220bb5c
Dave Roverts <me@daveroverts.nl>
2018-06-12 00:01:51 +0200

    Adds Event name in booking.overview

--- resources/views/booking/create.blade.php
+++ resources/views/booking/create.blade.php
@@ -18,6 +18,17 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('booking.create',$event->id) }}">
                         @csrf
+
+                        {{--Event--}}
+                        <div class="form-group row">
+                            <label for="event" class="col-md-4 col-form-label text-md-right">Event</label>
+
+                            <div class="col-md-6">
+                                <div class="form-control-plaintext">{{ $event->name }}</div>
+
+                            </div>
+                        </div>
+
                     </form>
                 </div>
             </div>
