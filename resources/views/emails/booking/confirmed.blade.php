a2bb8dbc67189f0bf69d44079e6b1e90b3b11fe7
Dave Roverts <me@daveroverts.nl>
2018-06-13 22:29:34 +0200

    Puts data now into a table

--- resources/views/emails/booking/confirmed.blade.php
+++ resources/views/emails/booking/confirmed.blade.php
@@ -6,14 +6,17 @@
     For reference, your booking details are listed below. On the day of the event, you will receive an email containing your route, oceanic flight level and slot time based on the appropriate NAT tracks.
     You will also be able to view this route upon login to the Dutch VACC booking page.
 
-    Callsign: {{ $booking->callsign }}
-    Departs: {{ $booking->dep  }}
-    Arrives: {{ $booking->arr }}
-    Cruising: {{ $booking->oceanicFL }}
-    SELCAL: {{ $booking->selcal }}
-    CTOT: {{ $booking->ctot }}
-
-    Event Date: {{ $booking->event->startEvent->toFormattedDateString() }}
+    @component('mail::table')
+        |  |  |
+        |-----------|---------------------------|
+        | Callsign: | {{ $booking->callsign }} |
+        | Departs: | {{ $booking->dep  }} |
+        | Arrives: | {{ $booking->arr }} |
+        | Cruising: | {{ $booking->oceanicFL }} |
+        | SELCAL: | {{ $booking->selcal }} |
+        | CTOT: | {{ $booking->ctot }} |
+        | Event Date: | {{ $booking->event->startEvent->toFormattedDateString() }} |
+    @endcomponent
 
     Regards,
     Dutch VACC
