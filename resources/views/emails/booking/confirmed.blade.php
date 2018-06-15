aa72ad97360768585aa5d5f37f2a52830c089863
Dave Roverts <me@daveroverts.nl>
2018-06-15 21:09:40 +0200

    Fixes issue where MD marked everything as code

--- resources/views/emails/booking/confirmed.blade.php
+++ resources/views/emails/booking/confirmed.blade.php
@@ -1,23 +1,25 @@
 @component('mail::message')
-    # Booking confirmed
+# Booking confirmed
 
-    Dear {{ $booking->bookedBy->full_name }},
-    Thank you for your recent booking for {{ $booking->event->name }} event.
-    For reference, your booking details are listed below. On the day of the event, you will receive an email containing your route, oceanic flight level and slot time based on the appropriate NAT tracks.
-    You will also be able to view this route upon login to the Dutch VACC booking page.
+Dear **{{ $booking->bookedBy->full_name }}**,
 
-    @component('mail::table')
-        |  |  |
-        |-----------|---------------------------|
-        | Callsign: | {{ $booking->callsign }} |
-        | Departs: | {{ $booking->dep  }} |
-        | Arrives: | {{ $booking->arr }} |
-        | Cruising: | {{ $booking->oceanicFL }} |
-        | SELCAL: | {{ $booking->selcal }} |
-        | CTOT: | {{ $booking->ctot }} |
-        | Event Date: | {{ $booking->event->startEvent->toFormattedDateString() }} |
-    @endcomponent
+Thank you for your recent booking for **{{ $booking->event->name }}** event.
+For reference, your booking details are listed below. On the day of the event, you will receive an email containing your route, oceanic flight level and slot time based on the appropriate NAT tracks.
+You will also be able to view this route upon login to the Dutch VACC booking page.
 
-    Regards,
-    Dutch VACC
+@component('mail::table')
+|  |  |
+|-----------|---------------------------|
+| Callsign: | **{{ $booking->callsign }}** |
+| Departs: | {{ $booking->dep  }} |
+| Arrives: | {{ $booking->arr }} |
+| Cruising: | {{ $booking->oceanicFL }} |
+| SELCAL: | {{ $booking->selcal }} |
+| CTOT: | **{{ $booking->ctot }}** |
+| Event Date: | {{ $booking->event->startEvent->toFormattedDateString() }} |
+@endcomponent
+
+Regards,
+
+**Dutch VACC**
 @endcomponent
