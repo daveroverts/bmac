e811d8c85634644e14771c9f4cce6d7b8326487b
Dave Roverts <me@daveroverts.nl>
2018-06-28 21:42:06 +0200

    Changes LoginController@logout() redirect to '/'

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -93,7 +93,7 @@ class LoginController extends Controller
     public function logout()
     {
         Auth::logout();
-        return redirect('/booking');
+        return redirect('/');
     }
 
 }
