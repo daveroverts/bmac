bb78bd47fcac23a2eeb92b2bf26d212d4952e31c
Dave Roverts <me@daveroverts.nl>
2018-06-30 19:42:08 +0200

    Adds Contact Us button, redirects to E-mail adres

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -16,6 +16,7 @@
                     <li class="nav-item {{ Request::is('booking/*') ? 'active' : '' }}"><a class="nav-link" href="">My bookings</a></li>
                 @endauth
                 <li class="nav-item" {{ Request::is('faq') ? 'active' : '' }}><a class="nav-link" href="{{ route('faq') }}">FAQ</a></li>
+                <li class="nav-item"><a class="nav-link" href="mailto:events@dutchvacc.nl">Contact Us</a></li>
                 @guest
                     <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">Login</a></li>
                 @else
