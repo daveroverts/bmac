b5cf339ce58fda568280d0cf1591d3789817735a
Dave Roverts <me@daveroverts.nl>
2018-07-15 23:10:00 +0200

    Changes one comment in booking.admin.edit

--- resources/views/booking/admin/edit.blade.php
+++ resources/views/booking/admin/edit.blade.php
@@ -166,7 +166,7 @@
                             </div>
                         </div>
 
-                        {{--Add--}}
+                        {{--Update--}}
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
