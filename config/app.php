41904ddf46ddb2eef5f6d95e09895a805c9caa51
Dave Roverts <me@daveroverts.nl>
2018-07-24 01:04:10 +0200

    Registers BugSnag

--- config/app.php
+++ config/app.php
@@ -151,7 +151,7 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Bugsnag\BugsnagLaravel\BugsnagServiceProvider::class,
         /*
          * Application Service Providers...
          */
