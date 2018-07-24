5858b07956a66107848ecc7298450189a51d88e6
Dave Roverts <me@daveroverts.nl>
2018-07-24 22:27:09 +0200

    Adds flashMessage() helper function

--- app/Helper.php
+++ app/Helper.php
@@ -1,2 +1,14 @@
 <?php
 
+/**
+ * Flash a message that can be used in layouts.alert
+ *
+ * @param $type
+ * @param $title
+ * @param $message
+ */
+function flashMessage($type, $title, $message){
+    Session::flash('type', $type);
+    Session::flash('title', $title);
+    Session::flash('message', $message);
+}
