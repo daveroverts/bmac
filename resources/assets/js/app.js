af90ca5be8f326c21948eb7cd0e0dd6b3f5fa9d7
Dave Roverts <me@daveroverts.nl>
2018-07-25 19:27:32 +0200

    Disables Vue.js again
    Why did I enable it if it wasn't really used at this time?

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -29,9 +29,9 @@ import brands from '@fortawesome/fontawesome-free-brands'
 fontawesome.library.add(regular, solid, brands);
 
 
-window.Vue = require('vue');
-import VueSweetalert2 from 'vue-sweetalert2';
-Vue.use(VueSweetalert2);
+// window.Vue = require('vue');
+// import VueSweetalert2 from 'vue-sweetalert2';
+// Vue.use(VueSweetalert2);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -39,8 +39,8 @@ Vue.use(VueSweetalert2);
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
-
-const app = new Vue({
-    el: '#app'
-});
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
+//
+// const app = new Vue({
+//     el: '#app'
+// });
