f6d5253f2fca685e624735ea439d510b268f9412
Dave Roverts <me@daveroverts.nl>
2018-07-30 21:04:20 +0200

    Makes sure that uuid's are generated when new Booking is created

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -9,6 +9,16 @@ class Booking extends Model
 {
 
     /**
+     *  Setup model event hooks
+     */
+    public static function boot()
+    {
+        parent::boot();
+        self::creating(function ($model) {
+            $model->uuid = (string) Uuid::generate(4);
+        });
+    }
+
     /**
      * Get the route key for the model.
      *
