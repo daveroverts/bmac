f3fee502fc0be64a2af066da0b862190859dd831
Dave Roverts <me@daveroverts.nl>
2018-08-04 15:17:54 +0200

    Adds auth.isAdmin middleware to AirportLinkController
    This controller will only be used by administrators. Later on, users can open up information on booking.show.

--- app/Http/Controllers/AirportLinkController.php
+++ app/Http/Controllers/AirportLinkController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 class AirportLinkController extends Controller
 {
     /**
+     * Instantiate a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth.isAdmin');
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
