01b4a66f4c97da914158028103c71beaeb1c3c3e
Dave Roverts <me@daveroverts.nl>
2018-08-20 22:39:05 +0200

    Adds testItCreatesNewEvent() in EventTest

--- tests/Unit/EventTest.php
+++ tests/Unit/EventTest.php
@@ -2,19 +2,48 @@
 
 namespace Tests\Unit;
 
+use App\Models\Event;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class EventTest extends TestCase
 {
+    use RefreshDatabase;
+
     /**
-     * A basic test example.
+     * Test if a event can be added
      *
      * @return void
      */
-    public function testExample()
+    public function testItCreatesNewEvent()
     {
-        $this->assertTrue(true);
+        $name = 'Amsterdam - Eelde Fly-In';
+        $description = 'Si, Fly';
+        $startEvent = now()->addMonth()->toDateTimeString();
+        $endEvent = now()->addMonth()->addHours(3)->toDateTimeString();
+        $startBooking = now()->addWeek()->toDateTimeString();
+        $endBooking = now()->subDay()->toDateTimeString();
+        $sendFeedbackForm = now()->addMonth()->addHours(2)->addDay()->toDateTimeString();
+
+        Event::create([
+            'name' => $name,
+            'description' => $description,
+            'startEvent' => $startEvent,
+            'endEvent' => $endEvent,
+            'startBooking' => $startBooking,
+            'endBooking' => $endBooking,
+            'sendFeedbackForm' => $sendFeedbackForm,
+        ]);
+
+        $this->assertDatabaseHas('events', [
+            'name' => $name,
+            'description' => $description,
+            'startEvent' => $startEvent,
+            'endEvent' => $endEvent,
+            'startBooking' => $startBooking,
+            'endBooking' => $endBooking,
+            'sendFeedbackForm' => $sendFeedbackForm,
+        ]);
     }
 }
