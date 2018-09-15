b104bcbdfb00261b2a5f6c1ecf862f931611ba23
Dave Roverts <me@daveroverts.nl>
2018-09-15 14:49:58 +0200

    Adds some actual SELCAL validation

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -203,8 +203,7 @@ class BookingController extends Controller
                 'callsign' => $request->callsign,
                 'acType' => $request->aircraft,
             ]);
-
-            $booking->selcal = $this->validateSELCAL($request->selcal1 . '-' . $request->selcal2, $booking->event_id);
+            $booking->selcal = $this->validateSELCAL($request->selcal1 . $request->selcal2, $booking->event_id);
             if ($booking->getOriginal('status') === BookingStatus::Reserved) {
                 Mail::to(Auth::user())->send(new BookingConfirmed($booking));
                 flashMessage('success', 'Booking created!', 'Booking has been created! A E-mail with details has also been sent');
@@ -229,6 +228,28 @@ class BookingController extends Controller
 
     public function validateSELCAL($selcal, $eventId)
     {
+        // Separate characters
+        $char1 = substr($selcal, 0, 1);
+        $char2 = substr($selcal, 1, 1);
+        $char3 = substr($selcal, 3, 1);
+        $char4 = substr($selcal, 4, 1);
+
+        // Check if SELCAL has valid format
+        if (!preg_match("/[ABCDEFGHJKLMPQRS][ABCDEFGHJKLMPQRS][-][ABCDEFGHJKLMPQRS][ABCDEFGHJKLMPQRS]/", $selcal)) {
+            return null;
+        }
+
+        // Check if each character is unique
+        if (substr_count($selcal, $char1) > 1 || substr_count($selcal, $char2) > 1 || substr_count($selcal, $char3) > 1 || substr_count($selcal, $char4) > 1 ) {
+            return null;
+        }
+
+        // Check if characters per pair are in alphabetical order
+        if ($char1 > $char2 || $char4 > $char3) {
+            return null;
+        }
+
+        // Check for duplicates within the same event
         if (Booking::where('event_id', $eventId)
             ->where('selcal', '=', $selcal)
             ->get()->isEmpty()) {
