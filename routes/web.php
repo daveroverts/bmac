daveroverts/bmac

commit 713e44c3ba8cf67f219a6477b442eb11c33d7e45

Adds optional $event should one want to access older/newer events
Also adds a link to event in event.overview page.

Closes #81
Related to #82

routes/web.php
@@ -28,6 +28,7 @@ Route::patch('/admin/event/{event}/email', 'EventController@sendEmail')->name('e
 Route::get('/admin/event/{event}/email_final', 'EventController@sendFinalInformationMail')->name('event.email.final');
 Route::resource('admin/event', 'EventController');
 
+Route::get('/booking/{event?}', 'BookingController@index')->name('booking.index');
 Route::get('/booking/{booking}/cancel', 'BookingController@cancel')->name('booking.cancel');
 Route::get('/booking/{event}/create', 'BookingController@create')->name('booking.create');
 Route::get('/booking/{event}/export', 'BookingController@export')->name('booking.export');
@@ -35,7 +36,7 @@ Route::get('/admin/booking/{booking}/edit', 'BookingController@adminEdit')->name
 Route::patch('/admin/booking/{booking}/edit', 'BookingController@adminUpdate')->name('booking.admin.update');
 Route::get('/admin/booking/{event}/autoAssign', 'BookingController@adminAutoAssignForm')->name('booking.admin.autoAssignForm');
 Route::patch('/admin/booking/{event}/autoAssign', 'BookingController@adminAutoAssign')->name('booking.admin.autoAssign');
-Route::resource('booking', 'BookingController')->except('create');
+Route::resource('booking', 'BookingController')->except('index', 'create');
 
 Route::get('/faq', function () {
     return view('faq');

app/Http/Controllers/BookingController.php
@@ -39,11 +39,14 @@ class BookingController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Event $event = null)
     {
         $this->removeOverdueReservations();
 
-        $event = Event::query()->where('endEvent', '>', now())->orderBy('startEvent', 'asc')->first();
+        //Check if specific event is requested, else fall back to current ongoing event
+        if (!$event)
+            $event = Event::where('endEvent', '>', now())->orderBy('startEvent', 'desc')->first();
+
         $bookings = collect();
 
         if($event)

app/Models/Event.php
@@ -73,4 +73,13 @@ class Event extends Model
         ];
     }
 
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }

resources/views/event/overview.blade.php
@@ -19,7 +19,7 @@
         @forelse($events as $event)
             <tr>
                 <td>{{ $event->id }}</td>
-                <td>{{ $event->name }}</td>
+                <td><a href="{{ route('booking.index', $event) }}">{{ $event->name }}</a></td>
                 <td>{{ $event->type->name }}</td>
                 <td>{{ $event->startEvent->format('d-m-Y') }}</td>
                 <td>{{ $event->startEvent->format('Hi') }}z</td>

commit 3e88c55e5dbb6f94c1f16320814cb0a7ffeeed7d

Moves resource stuff up
I learned today that you first need to define resource, then the rest. Bug with booking.show introduced in 713e44c3ba8cf67f219a6477b442eb11c33d7e45

routes/web.php
@@ -23,11 +23,12 @@ Route::resource('admin/airport', 'AirportController');
 
 Route::resource('admin/airportLink', 'AirportLinkController');
 
+Route::resource('admin/event', 'EventController');
 Route::get('/admin/event/{event}/email', 'EventController@sendEmailForm')->name('event.email.form');
 Route::patch('/admin/event/{event}/email', 'EventController@sendEmail')->name('event.email');
 Route::get('/admin/event/{event}/email_final', 'EventController@sendFinalInformationMail')->name('event.email.final');
-Route::resource('admin/event', 'EventController');
 
+Route::resource('booking', 'BookingController')->except(['create', 'index']);
 Route::get('/booking/{event?}', 'BookingController@index')->name('booking.index');
 Route::get('/booking/{booking}/cancel', 'BookingController@cancel')->name('booking.cancel');
 Route::get('/booking/{event}/create/{bulk?}', 'BookingController@create')->name('booking.create');
@@ -36,7 +37,6 @@ Route::get('/admin/booking/{booking}/edit', 'BookingController@adminEdit')->name
 Route::patch('/admin/booking/{booking}/edit', 'BookingController@adminUpdate')->name('booking.admin.update');
 Route::get('/admin/booking/{event}/autoAssign', 'BookingController@adminAutoAssignForm')->name('booking.admin.autoAssignForm');
 Route::patch('/admin/booking/{event}/autoAssign', 'BookingController@adminAutoAssign')->name('booking.admin.autoAssign');
-Route::resource('booking', 'BookingController')->except('index', 'create');
 
 Route::get('/faq', function () {
     return view('faq');