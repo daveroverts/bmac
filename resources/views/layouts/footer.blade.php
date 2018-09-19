e7781373252facecc9d4a04fe2259370ac570d60
Dave Roverts <me@daveroverts.nl>
2018-09-19 18:14:15 +0200

    Enables version in footer and composer again

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,5 +4,5 @@
     $thisYear = (int)date('Y');
     echo $fromYear . (($fromYear != $thisYear) ? '-' . $thisYear : '');?> <a href="https://www.dutchvacc.nl">Dutch VACC</a> Booking System, created by Dave Roverts - 1186831.
     <br>
-    {{--@version--}}
+    @version
 </footer>
