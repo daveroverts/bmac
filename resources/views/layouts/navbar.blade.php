5ef2c00a4986fca5e5058a372042f5a15e3cba3d
Dave Roverts <me@daveroverts.nl>
2018-09-25 21:03:21 +0200

    Changes My booking into multiple bookings if applicable
    Also removes the button if no event is active

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -18,10 +18,18 @@
                     <a class="nav-link" href="{{ route('booking.index') }}">Bookings</a>
                 </li>
                 @auth
-                    @if(Auth::user()->booking()->first())
-                        <li class="nav-item {{ Route::currentRouteNamed('booking.show') ? 'active' : '' }}">
-                            <a class="nav-link" href="{{ route('booking.show', Auth::user()->booking()->first()) }}">
-                                My booking</a></li>
+                    @if($event = App\Models\Event::where('endEvent', '>', now())->orderBy('startEvent', 'desc')->first())
+                        @foreach($bookings = Auth::user()->booking()->where('event_id',$event->id)->get() as $booking)
+                            @if($bookings->count() > 1)
+                                <li class="nav-item {{ url()->current() === route('booking.show', $booking) ? 'active' : '' }}">
+                                    <a class="nav-link" href="{{ route('booking.show', $booking) }}">
+                                        {{ $booking->callsign }}</a></li>
+                            @else
+                                <li class="nav-item {{ Route::currentRouteNamed('booking.show') ? 'active' : '' }}">
+                                    <a class="nav-link" href="{{ route('booking.show', $booking) }}">
+                                        My booking</a></li>
+                            @endif
+                        @endforeach
                     @endif
                 @endauth
                 {{--<li class="nav-item {{ Route::currentRouteNamed('faq') ? 'active' : '' }}">--}}
