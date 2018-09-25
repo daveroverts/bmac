0c1783662a1c8e2c688ad774ac6d5245e270e0ce
Dave Roverts <me@daveroverts.nl>
2018-09-25 22:32:00 +0200

    Reformats Middlewares

--- app/Http/Middleware/IsLoggedIn.php
+++ app/Http/Middleware/IsLoggedIn.php
@@ -3,17 +3,15 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\{
-    Support\Facades\Auth, Support\Facades\Session
-};
+use Illuminate\{Support\Facades\Auth};
 
 class IsLoggedIn
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
