fbb452340cf4543e2d7ffef5c12d6f9dfe115d8e
Dave Roverts <me@daveroverts.nl>
2018-10-10 20:09:40 +0000

    CRUD for AirportController + AirportLinkController

---
+++ resources/views/airportLink/overview.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+    <h2>Airport Links Overview</h2>
+    @include('layouts.alert')
+    <p>
+        <a href=" {{ route('airportLink.create') }}" class="btn btn-primary"><i class="fa fa-plus"></i> Add new Airport
+            Link</a>
+    </p>
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th scope="row">Airport</th>
+            <th scope="row">Type / Name</th>
+            <th scope="row" colspan="2">Actions</th>
+        </tr>
+        </thead>
+        @forelse($airportLinks as $airportLink)
+            <tr>
+                <td><a href="{{ route('airport.show', $airportLink->airport) }}">{{ $airportLink->airport->name }} [{{ $airportLink->airport->icao }} | {{ $airportLink->airport->iata }}]</a></td>
+                <td><a href="{{ $airportLink->url }}">{{ $airportLink->name ?? $airportLink->type->name }}</a></td>
+                <td>
+                    <a href="{{ route('airportLink.edit', $airportLink) }}">
+                        <button class="btn btn-primary">
+                            <i class="fa fa-edit"></i> Edit Airport Link
+                        </button>
+                    </a>
+                </td>
+                <td>
+                    <form action="{{ route('airportLink.destroy', $airportLink) }}" method="post">
+                        @method('DELETE')
+                        <button class="btn btn-danger"><i class="fa fa-trash"></i> Remove Airport Link</button>
+                        @csrf
+                    </form>
+                </td>
+            </tr>
+        @empty
+            @component('layouts.alert.warning')
+                @slot('title')
+                    No airport links found
+                @endslot
+                No airport links are in the system, consider adding one, using the button above
+            @endcomponent
+        @endforelse
+        {{ $airportLinks->links() }}
+    </table>
+    {{ $airportLinks->links() }}
+@endsection
