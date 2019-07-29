51adac1bc09a7cf2658db11ed0eb1367542cae3e
Dave Roverts <me@daveroverts.nl>
2019-07-29 20:35:51 +0200

    Reverting code to it's original stage (SKIP DEPLOYMENT)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,8 +12,7 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-    {
-        Schema::defaultStringLength(191);
+    {//
     }
 
     /**
