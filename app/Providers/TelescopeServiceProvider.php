9e27eca3890305153747793c2b73ea2eefdc3fd5
Dave Roverts <roverts.dave@gmail.com>
2019-08-01 16:18:55 +0000

    BMAC-252 Fix for telescope not working, despite being admin

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -62,7 +62,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return $user->is_admin;
+            return $user->isAdmin;
         });
     }
 }
