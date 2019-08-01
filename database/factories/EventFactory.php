62192303b400c17dc3ac0240418169bd39d5fb23
Dave Roverts <me@daveroverts.nl>
2019-08-01 13:43:00 +0200

    BMAC-244 Fix for slug not always being correct. Also made endBooking more logical

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -1,16 +1,18 @@
 <?php
 
+use App\Models\Event;
+use Cviebrock\EloquentSluggable\Services\SlugService;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 
 $factory->define(App\Models\Event::class, function (Faker $faker) {
-    $name = $faker->name;
+    $name = $faker->sentence;
+    $slug = SlugService::createSlug(Event::class, 'slug', $name);
     return [
         'name' => $name,
-        'slug' => Str::slug($name),
+        'slug' => $slug,
         'description' => $faker->text(),
         'dep' => function () {
-        return factory(App\Models\Airport::class)->create()->id;
+            return factory(App\Models\Airport::class)->create()->id;
         },
         'arr' => function () {
             return factory(App\Models\Airport::class)->create()->id;
@@ -18,6 +20,6 @@ $factory->define(App\Models\Event::class, function (Faker $faker) {
         'startEvent' => now()->addMonth()->toDateTimeString(),
         'endEvent' => now()->addMonth()->addHours(3)->toDateTimeString(),
         'startBooking' => now()->addWeek()->toDateTimeString(),
-        'endBooking' => now()->subDay()->toDateTimeString(),
+        'endBooking' => now()->addMonth()->subHours(12)->toDateTimeString(),
     ];
 });
