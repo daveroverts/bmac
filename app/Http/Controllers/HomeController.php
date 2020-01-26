bb76e3d3e3a0b5da683d193ae3d6cd9d0f819c93
Dave Roverts <me@daveroverts.nl>
2020-01-26 14:36:45 +0100

    Removed the temp check thingy

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,13 +15,6 @@ class HomeController extends Controller
     public function __invoke()
     {
         $events = nextEvents(false, false, true);
-        // @TODO Temporary. Maybe it's a good idea to also implement a order_by for events
-        // @TODO Remove this check after EUD Event (2019-01-18)
-        if ($events->first()->event_type_id == EventType::MULTIFLIGHTS) {
-            $tempEvent = $events->first();
-            $events->pull(0);
-            $events->push($tempEvent);
-        }
         return view('home', compact('events'));
     }
 }
