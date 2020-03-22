87e1a9a7cab75ae3bf94b119a124d6dbd30b3ccb
blt950 <blt950@users.noreply.github.com>
2020-03-22 14:56:08 +0100

    Added base URLs to mix() so it works in subfolders

--- config/app.php
+++ config/app.php
@@ -53,6 +53,7 @@ return [
     */
 
     'url' => env('APP_URL', 'http://localhost'),
+    'mix_url' => env('APP_URL', 'http://localhost'),
 
     /*
     |--------------------------------------------------------------------------
