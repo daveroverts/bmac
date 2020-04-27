1fb56c8a5220d9b6b83d011fbc9ff4ecf9abf624
blt950 <blt950@users.noreply.github.com>
2020-04-27 20:58:31 +0200

    Made bootstrap colors configurable
    Fallback is implemented in the mix, as app.php serves for back-end, no point having anything there for front-end.

--- config/app.php
+++ config/app.php
@@ -72,9 +72,6 @@ return [
     'pilotbrief_url' => env('SITE_PILOTBRIEF_URL', 'https://vaccdivision.org/brief'),
     'charts_url' => env('SITE_CHARTS_URL', 'https://vaccdivision.org/charts'),
 
-    'color_navbar' => env('SITE_COLOR_NAVBAR', '#2C3E50'),
-    'color_links' => env('SITE_COLOR_LINKS', '#18BC9C'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
