31517c62cf071eabc499e63c52031b3f572e728d
blt950 <blt950@users.noreply.github.com>
2020-06-09 22:19:58 +0200

    Reduced and fixed scopes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -82,8 +82,7 @@ class LoginController extends Controller
         if (
             ! isset($resourceOwner->data) ||
             ! isset($resourceOwner->data->cid) ||
-            ! isset($resourceOwner->data->personal) ||
-            $resourceOwner->data->oauth->token_valid !== "true"
+            ! isset($resourceOwner->data->personal)
         ) {
             flashMessage('error', 'Login failed', 'We need you to grant us all marked permissions');
             return redirect('/')->withError("We need you to grant us all marked permissions");
