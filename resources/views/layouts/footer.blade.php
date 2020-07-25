d3eb82490cbf3b6d0c87121cc9c40fd6c452d898
Dave Roverts <me@daveroverts.nl>
2020-07-25 15:57:37 +0200

    Trigger

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,5 +4,7 @@
     $thisYear = (int) date('Y');
     echo $fromYear.(($fromYear != $thisYear) ? '-'.$thisYear : '');?> Booking System by Dave Roverts (1186831). Used and maintained by <a href="{{ config('app.division_url') }}" target="_blank">{{ config('app.division') }}</a>
     <br>
+    Test
+    <br>
     <a class="navbar-brand" href="{{ config('app.division_url') }}"><img src="{{ asset('images/division-horizontal.png') }}" height="45"></a>
 </footer>
