f91865d17adfef0f64e2b17aaa8a318dec115d79
blt950 <blt950@users.noreply.github.com>
2020-07-26 23:46:44 +0200

    Made it OAuth agonostic
    Not everyone will use Vatsim Connect directly, but their own core login system that might not have the exact same data structure. This update allows to specify the data structure in the .env and makes it technically work with any OAuth2 provider. This will make it work with VATSCA's and VATUK's login systems to mentioned some.

---
+++ app/Http/Controllers/OAuthController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use League\OAuth2\Client\Token;
+use Illuminate\Support\Facades\Auth;
+use League\OAuth2\Client\Provider\GenericProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+
+class OAuthController extends GenericProvider
+{
+    /**
+     * @var GenericProvider
+     */
+    private $provider;
+
+    /**
+     * Initializes the provider variable.
+     */
+    public function __construct()
+    {
+        parent::__construct([
+            'clientId'                => config('oauth.id'),    // The client ID assigned to you by the provider
+            'clientSecret'            => config('oauth.secret'),   // The client password assigned to you by the provider
+            'redirectUri'             => route('login'),
+            'urlAuthorize'            => config('oauth.base').'/oauth/authorize',
+            'urlAccessToken'          => config('oauth.base').'/oauth/token',
+            'urlResourceOwnerDetails' => config('oauth.base').'/api/user',
+            'scopes'                  => config('oauth.scopes'),
+            'scopeSeparator'          => ' '
+        ]);
+    }
+
+    /**
+     * Gets an (updated) user token
+     * @param Token $token
+     * @return Token
+     * @return null
+     */
+    public static function updateToken($token)
+    {
+        $controller = new OAuthController;
+        try {
+            return $controller->getAccessToken('refresh_token', [
+                'refresh_token' => $token->getRefreshToken()
+            ]);
+        } catch (IdentityProviderException $e) {
+            return null;
+        }
+    }
+
+    // Thanks to Moodle for this snippet
+    // https://github.com/moodle/moodle/blob/48ad73619f870e4fd87240bd3c74202a300da6b2/lib/classes/oauth2/client.php#L255
+    public static function getOAuthProperty($property, $data){
+
+        $getfunc = function($obj, $prop) use (&$getfunc) {
+            $proplist = explode('-', $prop, 2);
+            if (empty($proplist[0]) || empty($obj->{$proplist[0]})) {
+                return false;
+            }
+            $obj = $obj->{$proplist[0]};
+
+            if (count($proplist) > 1) {
+                return $getfunc($obj, $proplist[1]);
+            }
+            return $obj;
+        };
+
+        $resolved = $getfunc($data, $property);
+        if (!empty($resolved)) {
+            return $resolved;
+        }
+
+        return false;
+        
+        /*
+        $getVar = function($var, $previous = null){
+            if(isset($previous)){
+                if(!isset($previous->${$var})){
+                    return false;
+                }
+                return $previous->${$var};
+            } else {
+                if(!isset(${$var})){
+                    return false;
+                }
+                return ${$var};
+            }
+        };
+
+        $properties = explode('-', $map);
+        $result = null;
+        for($x = 0; $x <= count($properties); $x++) {
+            $result = $getVar($properties[$x], $result);
+        }*/
+
+    }
+}
--- config/oauth.php
+++ config/oauth.php
@@ -14,24 +14,32 @@
 return [
 
     /*
-     * The location of the VATSIM OAuth interface
+     * The location of the OAuth interface
      */
-    'base' => env('VATSIM_OAUTH_BASE', 'https://auth.vatsim.net'),
+    'base' => env('OAUTH_BASE', 'https://auth.vatsim.net'),
 
     /*
      * The consumer key for your organisation (provided by VATSIM)
      */
-    'id' => env('VATSIM_OAUTH_CLIENT'),
+    'id' => env('OAUTH_CLIENT'),
 
     /*
     * The secret key for your organisation (provided by VATSIM)
     * Do not give this to anyone else or display it to your users. It must be kept server-side
     */
-    'secret' => env('VATSIM_OAUTH_SECRET'),
+    'secret' => env('OAUTH_SECRET'),
 
     /**
      * The scopes the user will be requested
      */
     'scopes' => explode(',', "full_name,email"),
+
+    /*
+     * OAuth variable mapping
+     */
+    'mapping_cid' => env('OAUTH_MAPPING_CID', 'data-cid'),
+    'mapping_first_name' => env('OAUTH_MAPPING_FIRSTNAME', 'data-personal-name_first'),
+    'mapping_last_name' => env('OAUTH_MAPPING_LASTNAME', 'data-personal-name_last'),
+    'mapping_mail' => env('OAUTH_MAPPING_EMAIL', 'data-personal-email'),
 
 ];
