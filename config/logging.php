a2eb6e69fa92cd68e4ffa11e03b8a5bbc3547c05
Dave Roverts <me@daveroverts.nl>
2020-11-17 09:50:55 +0100

    Use daily instead of single in stack

--- config/logging.php
+++ config/logging.php
@@ -35,7 +35,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single', 'bugsnag'],
+            'channels' => ['daily', 'bugsnag'],
         ],
 
         'single' => [
