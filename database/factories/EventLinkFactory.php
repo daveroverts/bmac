92859f8632b9e2e4f81675ea0263a76d8d463444
Dave Roverts <me@daveroverts.nl>
2021-01-29 12:07:02 +0100

    Don't forget the factory + unit test

---
+++ database/factories/EventLinkFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Airport;
+use App\Models\EventLink;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class EventLinkFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = EventLink::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'event_id' => Airport::factory(),
+            'event_link_type_id' => 4,
+            'url' => $this->faker->url,
+        ];
+    }
+}
