2e22129b6466cd21a724d934f9cd011f47ead640
Dave Roverts <me@daveroverts.nl>
2021-04-11 16:46:43 +0200

    Copied stuff from old wrapper

--- deploy.php
+++ deploy.php
@@ -23,11 +23,58 @@ host('104.248.84.152')
 
 // Tasks
 
-task('build', function () {
-    cd('{{release_path}}');
-    run('npm run build');
-});
-
 after('deploy:update_code', 'artisan:migrate');
 
 after('deploy:failed', 'deploy:unlock');
+
+desc('Upload Strategy');
+task('strategy:upload', [
+    'hook:start',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'upload',
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'hook:ready',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'hook:done',
+]);
+
+/**
+ * Strategy specific options
+ */
+
+set('upload_path', __DIR__ . '/../../../../..');
+
+set('upload_vendors', false);
+
+set('upload_options', function () {
+    $options = [
+        '--exclude=.git',
+        '--exclude=node_modules',
+    ];
+
+    if (! get('upload_vendors')) {
+        $options[] = '--exclude=/vendor';
+    }
+
+    return compact('options');
+});
+
+/**
+ * Strategy specific tasks
+ */
+
+desc('Upload a given folder to your hosts');
+task('upload', function () {
+    $configs = array_merge_recursive(get('upload_options'), [
+        'options' => ['--delete']
+    ]);
+
+    upload('{{upload_path}}/', '{{release_path}}', $configs);
+});
