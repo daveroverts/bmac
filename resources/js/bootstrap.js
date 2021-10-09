0048ecd5b7b2600ef788e040bc13503f6f89ed3a
Dave Roverts <me@daveroverts.nl>
2021-10-09 21:28:31 +0200

    Re-add popper.js

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,6 @@
+
+window.Popper = require('popper.js').default;
+
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
