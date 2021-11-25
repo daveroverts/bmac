133090b71169f1dabef21e749fb660d16c8fab7d
Dave Roverts <me@daveroverts.nl>
2021-11-25 23:23:26 +0100

    Almost

--- database/migrations/2018_06_08_163042_add_foreign_keys_to_bookings_table.php
+++ database/migrations/2018_06_08_163042_add_foreign_keys_to_bookings_table.php
@@ -16,8 +16,6 @@ class AddForeignKeysToBookingsTable extends Migration
         Schema::table('bookings', function (Blueprint $table) {
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('dep')->references('id')->on('airports');
-            $table->foreign('arr')->references('id')->on('airports');
         });
     }
 
@@ -30,10 +28,7 @@ class AddForeignKeysToBookingsTable extends Migration
     {
         Schema::table('bookings', function (Blueprint $table) {
             $table->dropForeign(['event_id']);
-            $table->dropForeign(['reservedBy_id']);
-            $table->dropForeign(['bookedBy_id']);
-            $table->dropForeign(['dep']);
-            $table->dropForeign(['arr']);
+            $table->dropForeign(['user_id']);
         });
     }
 }
