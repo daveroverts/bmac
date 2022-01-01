a4a064de2fe86ec377550e88c9af610bdf050b44
Dave Roverts <me@daveroverts.nl>
2022-01-01 11:18:10 +0100

    feat: move tinymce to separate js file
    -

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,16 +16,3 @@ import { fab } from '@fortawesome/free-brands-svg-icons'
 library.add(far, fas, fab);
 
 dom.watch();
-
-// TinyMCE
-import tinymce from 'tinymce';
-import 'tinymce/icons/default';
-import 'tinymce/themes/silver';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/paste';
-import 'tinymce/plugins/link';
-
-tinymce.init({
-    selector: 'textarea.tinymce',
-    plugins: ['code', 'paste', 'link']
-});
