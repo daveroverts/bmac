f261c518a2faa561fc65a9a149dae7055656177b
Dave Roverts <me@daveroverts.nl>
2022-01-02 13:48:04 +0100

    feat(AirportsImport): add better validation to airport import
    In case icao or iata are empty, skip it

--- app/Imports/AirportsImport.php
+++ app/Imports/AirportsImport.php
@@ -3,22 +3,27 @@
 namespace App\Imports;
 
 use App\Models\Airport;
+use Illuminate\Validation\Rule;
 use Maatwebsite\Excel\Concerns\Importable;
+use Maatwebsite\Excel\Concerns\SkipsFailures;
+use Maatwebsite\Excel\Concerns\SkipsOnFailure;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithBatchInserts;
 use Maatwebsite\Excel\Concerns\WithChunkReading;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithUpserts;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class AirportsImport implements ToModel, WithBatchInserts, WithChunkReading, WithHeadingRow, WithUpserts
+class AirportsImport implements ToModel, WithBatchInserts, WithChunkReading, WithHeadingRow, WithUpserts, WithValidation, SkipsOnFailure
 {
     use Importable;
+    use SkipsFailures;
 
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new Airport([
@@ -26,6 +31,15 @@ class AirportsImport implements ToModel, WithBatchInserts, WithChunkReading, Wit
             'iata' => $row['iata'],
             'name' => $row['name'],
         ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'icao' => ['required', 'string', Rule::unique('airports', 'icao')],
+            'iata' => ['required', 'string', Rule::unique('airports', 'iata')],
+            'name' => ['required', 'string'],
+        ];
     }
 
     public function uniqueBy()
