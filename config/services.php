6066a3b8b70384c4aaac123dcf6ab79d8eb12f84
amosngSP <40349656+amosngSP@users.noreply.github.com>
2022-07-10 21:32:46 +0800

    Update services.php

--- config/services.php
+++ config/services.php
@@ -29,5 +29,11 @@ return [
         'secret' => env('AWS_SECRET_ACCESS_KEY'),
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
+    
+     'sendinblue' => [
+        // api-key or partner-key
+        'key_identifier' => env('SENDINBLUE_KEY_IDENTIFIER', 'api-key'),
+        'key' => env('SENDINBLUE_KEY'),
+    ],
 
 ];
