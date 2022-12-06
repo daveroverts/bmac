78021bdb3b6faa6a83af86318fc349973a5201f0
Dave Roverts <me@daveroverts.nl>
2022-12-06 19:36:46 +0100

    chore: add breeze with react + ssr + dark mode

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.jsx',
+            ssr: 'resources/js/ssr.jsx',
             refresh: true,
         }),
+        react(),
     ],
+    ssr: {
+        noExternal: ['@inertiajs/server'],
+    },
 });
