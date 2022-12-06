e8f48f518f2de81de4cd255e1f7a28ca33397e90
Dave Roverts <me@daveroverts.nl>
2022-12-06 19:57:45 +0100

    chore: make valet secure work correctly

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,37 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig, loadEnv } from 'vite'
+import laravel from 'laravel-vite-plugin'
 import react from '@vitejs/plugin-react';
+import fs from 'fs'
+import { resolve } from 'path'
+import { homedir } from 'os'
 
-export default defineConfig({
+export default defineConfig(({ command, mode }) => {
+  // Load current .env-file
+  const env = loadEnv(mode, process.cwd(), '')
+
+  // Set the host based on APP_URL
+  let host = new URL(env.APP_URL).host
+  let homeDir = homedir()
+  let serverConfig = {}
+
+  if (homeDir) {
+    serverConfig = {
+      https: {
+        key: fs.readFileSync(
+          resolve(homeDir, `.config/valet/Certificates/${host}.key`),
+        ),
+        cert: fs.readFileSync(
+          resolve(homeDir, `.config/valet/Certificates/${host}.crt`),
+        ),
+      },
+      hmr: {
+        host
+      },
+      host
+    }
+  }
+
+  return {
     plugins: [
         laravel({
             input: 'resources/js/app.jsx',
@@ -14,4 +43,6 @@ export default defineConfig({
     ssr: {
         noExternal: ['@inertiajs/server'],
     },
+    server: serverConfig
+  }
 });
