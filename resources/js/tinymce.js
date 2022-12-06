61616a2096a9f422bda141bda1c157b23163c59d
Dave Roverts <me@daveroverts.nl>
2022-12-06 19:26:25 +0100

    chore: remove everything

--- resources/js/tinymce.js
+++
@@ -1,9 +0,0 @@
-// TinyMCE
-import tinymce from 'tinymce';
-import 'tinymce/plugins/code';
-import 'tinymce/plugins/link';
-
-tinymce.init({
-    selector: 'textarea.tinymce',
-    plugins: ['code', 'link']
-});
--- resources/views/booking/overview.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<livewire:bookings :event="$event" />
-@endsection
