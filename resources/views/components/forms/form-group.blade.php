66139d37155239d9f6ea03d16f59165da1962757
Dave Roverts <me@daveroverts.nl>
2024-10-28 19:14:36 +0100

    chore: add help text and inline support for form-group

--- resources/views/components/forms/form-group.blade.php
+++ resources/views/components/forms/form-group.blade.php
@@ -1,9 +1,15 @@
-@props(['name', 'label' => null, 'inline' => false])
+@props(['name', 'label' => null, 'inline' => false, 'help'])
 
 <div class="form-group">
     <x-forms.label :for="$name" :label="$label" />
 
-    <div>
+    <div {{ $attributes->class(['form-inline' => $inline]) }}>
         {!! $slot !!}
     </div>
+
+    @isset($help)
+        <small id="{{ $name }}-help" class="form-text text-muted">
+            {!! $help !!}
+        </small>
+    @endisset
 </div>
