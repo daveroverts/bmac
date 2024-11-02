f63ee41c9f16e70c33c86a4dc757cb18c623001e
Dave Roverts <me@daveroverts.nl>
2024-11-02 14:44:42 +0100

    chore: add textarea

---
+++ resources/views/components/forms/textarea.blade.php
@@ -0,0 +1,19 @@
+@props(['name', 'label' => null, 'value' => null, 'help', 'tinymce' => true])
+
+<x-forms.form-group :name="$name" :label="$label">
+    <textarea name="{{ $name }}" id="{{ $name }}" @class([
+        'form-control',
+        'tinymce' => $tinymce,
+        'is-invalid' => $errors->has($name),
+    ])>
+        {{ old($name, $value) }}
+    </textarea>
+
+    <x-error :field="$name" class="invalid-feedback" />
+
+    @isset($help)
+        <small id="{{ $name }}-help" class="form-text text-muted">
+            {!! $help !!}
+        </small>
+    @endisset
+</x-forms.form-group>
