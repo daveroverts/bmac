b1dcd08dcc8542bb06c3f0bbed2282070f9737ff
Dave Roverts <me@daveroverts.nl>
2024-11-02 14:43:57 +0100

    chore: add placeholder to select

--- resources/views/components/forms/select.blade.php
+++ resources/views/components/forms/select.blade.php
@@ -1,7 +1,9 @@
-@props(['name', 'label' => null, 'value' => null, 'options', 'help'])
+@props(['name', 'label' => null, 'value' => null, 'options', 'placeholder' => null, 'help'])
 <x-forms.form-group :name="$name" :label="$label">
     <select name="{{ $name }}" {{ $attributes->class(['custom-select', 'is-invalid' => $errors->has($name)]) }}>
-
+        @if ($placeholder)
+            <option value="" disabled selected>{{ $placeholder }}</option>
+        @endif
 
         @foreach ($options as $key => $option)
             <option value="{{ $key }}" @selected(old($name, $value) == $key)>{{ $option }}</option>
