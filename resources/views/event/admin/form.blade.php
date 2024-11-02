80b8f7c044e2a24472deaa931543d20889e064bb
Dave Roverts <me@daveroverts.nl>
2024-11-02 14:45:02 +0100

    chore: convert event admin form

--- resources/views/event/admin/form.blade.php
+++ resources/views/event/admin/form.blade.php
@@ -9,104 +9,106 @@
 
                 <div class="card-body">
                     <x-form :action="$event->id ? route('admin.events.update', $event) : route('admin.events.store')" :method="$event->id ? 'PATCH' : 'POST'">
-                        @bind($event)
-                            <x-form-group name="is_online" :label="__('Show online?')" inline>
-                                <x-form-radio name="is_online" value="0" :label="__('No')" required />
-                                <x-form-radio name="is_online" value="1" :label="__('Yes')" required />
-                                @slot('help')
-                                    <small class="form-text text-muted">
-                                        {{ __("Choose here if you want the event to be reachable by it's generated url") }}
-                                    </small>
-                                @endslot
-                            </x-form-group>
+                        <x-forms.form-group name="is_online" :label="__('Show online?')" inline>
+                            <x-forms.radio name="is_online" value="0" :label="__('No')" inline required
+                                :checked="!$event->is_online" />
+                            <x-forms.radio name="is_online" value="1" :label="__('Yes')" inline required
+                                :checked="$event->is_online" />
+                            <x-slot:help>
+                                {{ __("Choose here if you want the event to be reachable by it's generated url") }}
+                            </x-slot:help>
+                        </x-forms.form-group>
 
-                            <x-form-group name="show_on_homepage" :label="__('Show on homepage?')" inline>
-                                <x-form-radio name="show_on_homepage" value="0" :label="__('No')" required />
-                                <x-form-radio name="show_on_homepage" value="1" :label="__('Yes')" required />
-                                @slot('help')
-                                    <small class="form-text text-muted">
-                                        {{ __("Choose here if you want to show the event on the homepage. If turned off, the event can only be reached by the url. NOTE: If 'Show Online' is off, the event won't be shown at all") }}
-                                    </small>
-                                @endslot
-                            </x-form-group>
+                        <x-forms.form-group name="show_on_homepage" :label="__('Show on homepage?')" inline>
+                            <x-forms.radio name="show_on_homepage" value="0" :label="__('No')" inline required
+                                :checked="!$event->show_on_homepage" />
+                            <x-forms.radio name="show_on_homepage" value="1" :label="__('Yes')" inline required
+                                :checked="$event->show_on_homepage" />
+                            <x-slot:help>
+                                {{ __("Choose here if you want to show the event on the homepage. If turned off, the event can only be reached by the url. NOTE: If 'Show Online' is off, the event won't be shown at all") }}
+                            </x-slot:help>
+                        </x-forms.form-group>
 
-                            <x-form-input name="name" :label="__('Name')" required />
+                        <x-forms.input name="name" :label="__('Name')" required :value="$event->name" />
 
-                            <x-form-select name="event_type_id" :label="__('Event type')" :options="$eventTypes" :placeholder="__('Choose...')"
-                                required />
+                        <x-forms.select name="event_type_id" :label="__('Event type')" :value="$event->event_type_id" :options="$eventTypes"
+                            :placeholder="__('Choose...')" required />
 
-                            <x-form-group name="import_only" :label="__('Only import?')" inline>
-                                <x-form-radio name="import_only" value="0" :label="__('No')" required />
-                                <x-form-radio name="import_only" value="1" :label="__('Yes')" required />
-                                @slot('help')
-                                    <small class="form-text text-muted">
-                                        {{ __('If enabled, only admins can fill in details via import script') }}
-                                    </small>
-                                @endslot
-                            </x-form-group>
+                        <x-forms.form-group name="import_only" :label="__('Only import?')" inline>
+                            <x-forms.radio name="import_only" value="0" :label="__('No')" inline required
+                                :checked="!$event->import_only" />
+                            <x-forms.radio name="import_only" value="1" :label="__('Yes')" inline required
+                                :checked="$event->import_only" />
+                            <x-slot:help>
+                                {{ __('If enabled, only admins can fill in details via import script') }}
+                            </x-slot:help>
+                        </x-forms.form-group>
 
-                            <x-form-group name="uses_times" :label="__('Show times?')" inline>
-                                <x-form-radio name="uses_times" value="0" :label="__('No')" required />
-                                <x-form-radio name="uses_times" value="1" :label="__('Yes')" required />
-                                @slot('help')
-                                    <small class="form-text text-muted">
-                                        {{ __('If enabled, CTOT and ETA (if set in booking) will be shown') }}
-                                    </small>
-                                @endslot
-                            </x-form-group>
+                        <x-forms.form-group name="uses_times" :label="__('Show times?')" inline>
+                            <x-forms.radio name="uses_times" value="0" :label="__('No')" inline required
+                                :checked="!$event->uses_times" />
+                            <x-forms.radio name="uses_times" value="1" :label="__('Yes')" inline required
+                                :checked="$event->uses_times" />
+                            <x-slot:help>
+                                {{ __('If enabled, CTOT and ETA (if set in booking) will be shown') }}
+                            </x-slot:help>
+                        </x-forms.form-group>
 
-                            <x-form-group name="multiple_bookings_allowed" :label="__('Multiple bookings allowed?')" inline>
-                                <x-form-radio name="multiple_bookings_allowed" value="0" :label="__('No')" required />
-                                <x-form-radio name="multiple_bookings_allowed" value="1" :label="__('Yes')" required />
-                                @slot('help')
-                                    <small class="form-text text-muted">
-                                        {{ __('If enabled, a user is allowed to book multiple flights for this event') }}
-                                    </small>
-                                @endslot
-                            </x-form-group>
+                        <x-forms.form-group name="multiple_bookings_allowed" :label="__('Multiple bookings allowed?')" inline>
+                            <x-forms.radio name="multiple_bookings_allowed" value="0" :label="__('No')" inline
+                                required :checked="!$event->multiple_bookings_allowed" />
+                            <x-forms.radio name="multiple_bookings_allowed" value="1" :label="__('Yes')" inline
+                                required :checked="$event->multiple_bookings_allowed" />
+                            <x-slot:help>
+                                {{ __('If enabled, a user is allowed to book multiple flights for this event') }}
+                            </x-slot:help>
+                        </x-forms.form-group>
 
-                            <x-form-group name="is_oceanic_event" :label="__('Oceanic event?')" inline>
-                                <x-form-radio name="is_oceanic_event" value="0" :label="__('No')" required />
-                                <x-form-radio name="is_oceanic_event" value="1" :label="__('Yes')" required />
-                                @slot('help')
-                                    <small class="form-text text-muted">
-                                        {{ __('If enabled, users can fill in a SELCAL code') }}
-                                    </small>
-                                @endslot
-                            </x-form-group>
+                        <x-forms.form-group name="is_oceanic_event" :label="__('Oceanic event?')" inline>
+                            <x-forms.radio name="is_oceanic_event" value="0" :label="__('No')" inline required
+                                :checked="!$event->is_oceanic_event" />
+                            <x-forms.radio name="is_oceanic_event" value="1" :label="__('Yes')" inline required
+                                :checked="$event->is_oceanic_event" />
+                            <x-slot:help>
+                                {{ __('If enabled, users can fill in a SELCAL code') }}
+                            </x-slot:help>
 
-                            <x-form-select name="dep" :label="__('Departure airport')" :options="$airports" :placeholder="__('Choose...')" required />
+                            <x-error field="is_oceanic_event" class="invalid-feedback" />
+                        </x-forms.form-group>
 
-                            <x-form-select name="arr" :label="__('Arrival airport')" :options="$airports" :placeholder="__('Choose...')" required />
+                        <x-forms.select name="dep" :label="__('Departure airport')" :value="$event->dep" :options="$airports"
+                            :placeholder="__('Choose...')" required />
 
-                            <x-form-group :label="__('Start event (UTC)')">
-                                <x-flat-pickr name="startEvent" value="{{ old('startEvent', $event->startEvent) }}" />
-                            </x-form-group>
+                        <x-forms.select name="arr" :label="__('Arrival airport')" :value="$event->arr" :options="$airports"
+                            :placeholder="__('Choose...')" required />
 
-                            <x-form-group :label="__('End event (UTC)')">
-                                <x-flat-pickr name="endEvent" value="{{ old('endEvent', $event->endEvent) }}" />
-                            </x-form-group>
+                        <x-forms.form-group name="startEvent" :label="__('Start event (UTC)')">
+                            <x-flat-pickr name="startEvent" value="{{ old('startEvent', $event->startEvent) }}" />
+                        </x-forms.form-group>
 
-                            <x-form-group :label="__('Start booking (UTC)')">
-                                <x-flat-pickr name="startBooking" value="{{ old('startBooking', $event->startBooking) }}" />
-                            </x-form-group>
+                        <x-forms.form-group name="endEvent" :label="__('End event (UTC)')">
+                            <x-flat-pickr name="endEvent" value="{{ old('endEvent', $event->endEvent) }}" />
+                        </x-forms.form-group>
 
-                            <x-form-group :label="__('End booking (UTC)')">
-                                <x-flat-pickr name="endBooking" value="{{ old('endBooking', $event->endBooking) }}" />
-                            </x-form-group>
+                        <x-forms.form-group name="startBooking" :label="__('Start booking (UTC)')">
+                            <x-flat-pickr name="startBooking" value="{{ old('startBooking', $event->startBooking) }}" />
+                        </x-forms.form-group>
 
-                            <x-form-input name="image_url" :label="__('Image URL')" placeholder="https://example.org" />
+                        <x-forms.form-group name="endBooking" :label="__('End booking (UTC)')">
+                            <x-flat-pickr name="endBooking" value="{{ old('endBooking', $event->endBooking) }}" />
+                        </x-forms.form-group>
 
-                            <x-form-textarea name="description" :label="__('Description')" class="tinymce" />
+                        <x-forms.input name="image_url" :label="__('Image URL')" placeholder="https://example.org" />
 
-                            <x-form-submit>
-                                @if ($event->id)
-                                    <i class="fa fa-check"></i> {{ __('Edit') }}
-                                @else
-                                    <i class="fa fa-plus"></i> {{ __('Add') }}
-                                @endif
+                        <x-forms.textarea name="description" :label="__('Description')" :value="$event->description" />
+
+                        <x-forms.button type="submit">
+                            @if ($event->id)
+                                <i class="fa fa-check"></i> {{ __('Edit') }}
+                            @else
+                                <i class="fa fa-plus"></i> {{ __('Add') }}
+                            @endif
                             </x-form-submit>
-                        @endbind
                     </x-form>
                 </div>
             </div>
