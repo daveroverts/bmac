9f1cded9c08a179182ee76948e29d0b2e4310549
Dave Roverts <me@daveroverts.nl>
2025-02-26 20:57:07 +0100

    chore: fix cache

--- rector.php
+++ rector.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use Rector\Caching\ValueObject\Storage\FileCacheStorage;
 use Rector\Config\RectorConfig;
 use RectorLaravel\Set\LaravelLevelSetList;
 
@@ -14,6 +15,13 @@ return RectorConfig::configure()
         __DIR__ . '/routes',
         __DIR__ . '/tests',
     ])
+    ->withCache(
+    // ensure file system caching is used instead of in-memory
+        cacheClass: FileCacheStorage::class,
+
+        // specify a path that works locally as well as on CI job runners
+        cacheDirectory: '/tmp/rector'
+    )
     ->withPhpSets()
     ->withTypeCoverageLevel(0)
     ->withSets([LaravelLevelSetList::UP_TO_LARAVEL_100]);
